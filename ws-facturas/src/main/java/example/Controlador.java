bd59f59d4baeb4452891d2c561a1237d75679231
raulsperoni <raulsperoni@gmail.com>
2015-08-28 15:31:33 -0300

    Ejemplo con WS funcionando.

---
+++ ws-facturas/src/main/java/example/Controlador.java
@@ -0,0 +1,11 @@
+package example;
+
+import javax.ws.rs.ApplicationPath;
+import javax.ws.rs.core.Application;
+
+/**
+ * Created by RSperoni on 28/08/2015.
+ */
+@ApplicationPath("/rest")
+public class Controlador extends Application {
+}
---
+++ ws-facturas/src/main/java/example/HelloWorld.java
@@ -0,0 +1,29 @@
+package example;
+import com.sun.net.httpserver.HttpServer;
+import com.sun.jersey.api.container.httpserver.HttpServerFactory;
+import java.io.IOException;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
+import javax.ws.rs.Path;
+import javax.ws.rs.core.MediaType;
+
+/**
+ * Created by RSperoni on 28/08/2015.
+ */
+// The Java class will be hosted at the URI path "/helloworld"
+@Path("/helloworld")
+public class HelloWorld {
+
+    //http://localhost:8080/ws-facturas/rest/helloworld/echo/r
+
+    @GET
+    @Path("echo/{echostr}")
+    @Produces(MediaType.TEXT_PLAIN)
+    public String echo(@PathParam("echostr") String echo) {
+        return echo;
+    }
+
+
+}
