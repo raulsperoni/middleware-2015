a4ad928027cf1f2024814864f85a3ad9126cef55
raul <raul@mgcoders.uy>
2015-09-09 16:51:11 -0300

    Pagina lealtad.

--- central/src/test/java/ProduceMessage.java
+++ central/src/test/java/ProduceMessage.java
@@ -3,7 +3,7 @@ import org.fing.middleware.jms.Producer;
 import org.junit.Test;
 
 import javax.jms.JMSException;
-import java.util.Random;
+import java.util.Date;
 
 /**
  * Created by raul on 03/09/15.
@@ -15,7 +15,9 @@ public class ProduceMessage {
         Producer p = null;
         try {
             p = new Producer();
-            p.send(new DataLealtad(new Random().nextInt(), "USD", 100, null));
+            p.send(new DataLealtad(1, DataLealtad.Monto.USD, 1000, new Date()));
+            p.send(new DataLealtad(2, DataLealtad.Monto.USD, 1345, new Date()));
+            p.send(new DataLealtad(3, DataLealtad.Monto.USD, 34567, new Date()));
         } catch (JMSException e) {
             e.printStackTrace();
         }
---
+++ lealtad/src/main/java/org/fing/middleware/beans/Usuario.java
@@ -0,0 +1,49 @@
+package org.fing.middleware.beans;
+
+import org.fing.middleware.jms.DataLealtad;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by raul on 09/09/15.
+ */
+public class Usuario {
+
+    private final long identificadorUsuario;
+    private int puntos = 0;
+    private List<DataLealtad> listaPagos = new ArrayList<DataLealtad>();
+
+    public Usuario(long identificadorUsuario) {
+        this.identificadorUsuario = identificadorUsuario;
+    }
+
+    public void addPago(DataLealtad d) {
+        this.listaPagos.add(d);
+        puntos += calcularPuntos(d);
+
+    }
+
+    public int calcularPuntos(DataLealtad d) {
+        switch (d.getCodigoMoneda()) {
+            case UYU:
+                return (int) d.getMonto() / 100;
+            case USD:
+                return (int) d.getMonto() / 10;
+            default:
+                return (int) d.getMonto() / 100;
+        }
+    }
+
+    public int getPuntos() {
+        return puntos;
+    }
+
+    public List<DataLealtad> getListaPagos() {
+        return listaPagos;
+    }
+
+    public long getIdentificadorUsuario() {
+        return identificadorUsuario;
+    }
+}
