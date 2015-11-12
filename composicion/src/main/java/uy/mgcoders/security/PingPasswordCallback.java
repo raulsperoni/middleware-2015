2e3354f9e3838539b2682d1ea9c67d8dab7b6bcd
Cristian Bauza <cristian.bauza.silva@gmail.com>
2015-11-11 22:40:11 -0300

    Todo el trabajo de pablito ahora paso al composicion

--- composicion/src/main/java/uy/mgcoders/security/PingPasswordCallback.java
+++ composicion/src/main/java/uy/mgcoders/security/PingPasswordCallback.java
@@ -1,43 +1,45 @@
 package uy.mgcoders.security;
 
-import org.apache.ws.security.WSPasswordCallback;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-
-import javax.security.auth.callback.Callback;
-import javax.security.auth.callback.CallbackHandler;
-import javax.security.auth.callback.UnsupportedCallbackException;
 import java.io.IOException;
 import java.util.HashMap;
 import java.util.Map;
 
-/**
- * Created by pablo on 11/7/15.
- */
-public class KeystorePasswordCallback implements CallbackHandler {
+import javax.security.auth.callback.Callback;
+import javax.security.auth.callback.CallbackHandler;
+import javax.security.auth.callback.UnsupportedCallbackException;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.apache.ws.security.WSPasswordCallback;
+
+public class PingPasswordCallback implements CallbackHandler {
+
+    private final static Logger s_logger = LoggerFactory.getLogger(PingPasswordCallback.class);
 
     private Map<String, String> passwords = new HashMap<String, String>();
-    private static final Logger logger = LoggerFactory.getLogger(KeystorePasswordCallback.class);
 
-    public KeystorePasswordCallback() {
+    public PingPasswordCallback() {
         passwords.put("server", "middleware2015");
         passwords.put("client", "middleware2015");
-        logger.debug("constructor - KeystorePasswordCallback");
     }
 
+    /**
+     * It attempts to get the password from the private
+     * alias/passwords map.
+     */
+    @Override
     public void handle(Callback[] callbacks) throws IOException, UnsupportedCallbackException {
         for (int i = 0; i < callbacks.length; i++) {
             WSPasswordCallback pc = (WSPasswordCallback)callbacks[i];
+            System.out.println("Requested password for " + pc.getIdentifier());
 
             String pass = passwords.get(pc.getIdentifier());
             if (pass != null) {
                 pc.setPassword(pass);
                 return;
+            } else {
+                pc.setPassword("password");
             }
         }
-    }
-
-    public void setAliasPassword(String alias, String password) {
-        passwords.put(alias, password);
     }
 }
