dac56bc425f99baa1a9b928e1ef98320e4e684cb
Pablo Olivera <pablor88@gmail.com>
2015-11-12 23:23:49 -0300

    renombrar el passwordcallback y sacar comentarios en ingles.

--- composicion/src/main/java/uy/mgcoders/security/ComposicionPasswordCallback.java
+++ composicion/src/main/java/uy/mgcoders/security/ComposicionPasswordCallback.java
@@ -12,27 +12,24 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.apache.ws.security.WSPasswordCallback;
 
-public class PingPasswordCallback implements CallbackHandler {
+public class ComposicionPasswordCallback implements CallbackHandler {
 
-    private final static Logger s_logger = LoggerFactory.getLogger(PingPasswordCallback.class);
+    private final static Logger logger = LoggerFactory.getLogger(ComposicionPasswordCallback.class);
 
     private Map<String, String> passwords = new HashMap<String, String>();
 
-    public PingPasswordCallback() {
+    public ComposicionPasswordCallback() {
         passwords.put("server", "middleware2015");
         passwords.put("client", "middleware2015");
+
+        logger.info("ComposicionPasswordCallback");
     }
 
-    /**
-     * It attempts to get the password from the private
-     * alias/passwords map.
-     */
     @Override
     public void handle(Callback[] callbacks) throws IOException, UnsupportedCallbackException {
         for (int i = 0; i < callbacks.length; i++) {
             WSPasswordCallback pc = (WSPasswordCallback)callbacks[i];
-            System.out.println("Requested password for " + pc.getIdentifier());
-
+            logger.info("handle indentifier.....: " + pc.getIdentifier());
             String pass = passwords.get(pc.getIdentifier());
             if (pass != null) {
                 pc.setPassword(pass);
